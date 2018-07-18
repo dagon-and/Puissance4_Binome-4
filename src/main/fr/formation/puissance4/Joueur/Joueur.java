a19f914c8db707a192858be17f9ace1535d6a0fc
lebbadfati <lebbad_fatima@yahoo.com>
2018-07-18 15:00:48 +0200

    J'ai implementé c'est deux methodes: public boolean setJeton(int ligne, int colonne) public void setJetonAdversaire(int ligne, int colonne, Color adversaireColor) Dans  la classe Joueur

--- src/main/fr/formation/puissance4/Joueur/Joueur.java
+++ src/main/fr/formation/puissance4/Joueur/Joueur.java
@@ -7,14 +7,14 @@ public abstract class Joueur {
     protected Color color;
     protected Board board;
     protected boolean isMsgError;
-    protected boolean  isDefaite;
+    protected boolean isDefaite;
 
 
     public Joueur(Color color, Board board) {
         this.color = color;
         this.board = board;
-        this.isMsgError=false;
-        this.isDefaite=false;
+        this.isMsgError = false;
+        this.isDefaite = false;
     }
 
     public boolean checkJetonPosiotion(int ligne, int colonne) {
@@ -33,12 +33,32 @@ public abstract class Joueur {
         return true;
     }
 
+    public boolean setJeton(int ligne, int colonne) {
+        if (checkJetonPosiotion(ligne, colonne)) {
+            board.getJetons()[ligne][colonne].setColor(color);
+            return true;
+        }
+        return false;
+
+    }
+
+    public void setJetonAdversaire(int ligne, int colonne, Color adversaireColor) {
+        if (checkJetonPosiotion(ligne, colonne)) {
+            board.getJetons()[ligne][colonne].setColor(color);
+
+        } else {
+            isMsgError = true;
+        }
+
+
+    }
+
     public boolean isFinish() {
         // vérification :
         // - s'il une erreur dans le message reçu
         // - si l'adversaire a gagné
         // - si le board est complètement rempli
-        if(isMsgError || isDefaite || board.isFullBoard()) {
+        if (isMsgError || isDefaite || board.isFullBoard()) {
             return true;
         }
         return false;
